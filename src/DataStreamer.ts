ddc4490290327048bd0507b68d1c216280742c16
o1-spec <oluwafemionadokun@gmail.com>
2024-09-23 01:47:37 +0100

    finished task 3

--- src/DataStreamer.ts
+++ src/DataStreamer.ts
@@ -1,28 +1,28 @@
 export interface Order {
-  price: number,
-  size: number,
+  price: number;
+  size: number;
 }
 export interface ServerRespond {
-  stock: string,
-  top_bid: Order,
-  top_ask: Order,
-  timestamp: Date,
+  stock: string;
+  top_bid: Order;
+  top_ask: Order;
+  timestamp: Date;
 }
 
 class DataStreamer {
-  static API_URL: string = 'http://localhost:8080/query?id=1';
+  static API_URL: string = "http://localhost:8080/query?id=1";
 
   static getData(callback: (data: ServerRespond[]) => void): void {
     const request = new XMLHttpRequest();
-    request.open('GET', DataStreamer.API_URL, false);
+    request.open("GET", DataStreamer.API_URL, false);
 
     request.onload = () => {
       if (request.status === 200) {
         callback(JSON.parse(request.responseText));
       } else {
-        alert ('Request failed');
+        alert("Request failed");
       }
-    }
+    };
 
     request.send();
   }
